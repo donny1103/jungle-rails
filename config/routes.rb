3506c5dd887f69eb8e75d5f89de742b5f874fdb4
donny1103 <donny1103@hotmail.com>
2018-10-10 21:04:16 +0000

    add signup and signin page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
 
   root to: 'products#index'
-
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
-
+  resources :users, only: [:new, :create]
   resource :cart, only: [:show] do
     post   :add_item
     post   :remove_item
@@ -17,7 +16,10 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
     resources :categories, only:[:index, :new, :create]
   end
-
+  
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
